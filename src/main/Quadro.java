b7355e5e51d1d048e30b75e27603d40d919edd9d
RAFAEL PEZZETTI PELLICCIOLI <ALU201210856@uniritter.local>
2017-12-08 20:44:52 -0200

    test quadro vazio

--- src/main/Quadro.java
+++ src/main/Quadro.java
@@ -4,20 +4,34 @@ public class Quadro {
 
 	private int linhas;
 	private int colunas;
+	private char [][] quadro;
 
 	public Quadro(int i, int j) {
 		linhas = i;
 		colunas = j;
+		quadro = new char[linhas][colunas];
+		for(int x = 0; x < linhas; x++){
+			for(int y = 0; y < colunas; y++){
+				quadro[x][y] = '-';
+					
+			}
+		}
 	}
 
 	public void desenha(int i, int j, char c) {
-		
-		
+		quadro[i][j] = c;
 	}
 
-	public Object existeGanhador() {
-		// TODO Auto-generated method stub
-		return true;
+	public boolean estaVazio() {
+		boolean vazio = true;
+		for(int x = 0; x < linhas; x++){
+			for(int y = 0; y < colunas; y++){
+				if(quadro[x][y] != '-'){
+					vazio = false;
+				}
+			}
+		}
+		return vazio;
 	}
 	
 
--- src/tests/TestTicTacToe.java
+++ src/tests/TestTicTacToe.java
@@ -14,11 +14,23 @@ public class TestTicTacToe {
 		// quando eu verificar se é um vencedor
 		// entao mostro o vencedor
 		Quadro quadro = new Quadro(3,3);
-		quadro.desenha(1,1,'X');
-		quadro.desenha(1,2,'X');
-		quadro.desenha(1,3,'X');
+		quadro.desenha(0,0,'X');
+		quadro.desenha(0,1,'X');
+		quadro.desenha(0,2,'X');
 		
-		assertEquals(true,quadro.existeGanhador());
+		assertEquals(false,quadro.estaVazio());
+		
+	}
+	
+	@Test
+	public void testQuadroVazio() {
+		// dado que o quadro tenha uma linha prenchida e todos elementos sao iguais
+		// quando eu verificar se é um vencedor
+		// entao mostro o vencedor
+		Quadro quadro = new Quadro(3,3);
+	
+		
+		assertEquals(true,quadro.estaVazio());
 		
 	}
 
